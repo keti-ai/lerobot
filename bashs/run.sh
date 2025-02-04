b13b8d3a9b7bf5598401af5f2214dced701e37e0
syh <syh20131292@gmail.com>
2025-02-04 14:06:02 +0900

    esit bahs

--- bashs/run.sh
+++ bashs/run.sh
@@ -1,8 +1,23 @@
-# cali
+## cali
+#cd ..
+#python lerobot/scripts/control_robot.py \
+#--robot.type=so100 \
+#--robot.cameras={} \
+#--control.type=calibrate
+#cd scripts
 
-# teleoperation
+## teleoperation
+#cd ..
+#python lerobot/scripts/control_robot.py \
+#--robot.type=so100 \
+#--robot.cameras={} \
+#--control.type=teleoperate
+#cd scripts
 
-# record
+## record
+recorder=syhlab
+exp_name=test_record
+exp_num=1
 cd ..
 python lerobot/scripts/control_robot.py \
 --robot.type=so100 \
@@ -10,19 +25,22 @@ python lerobot/scripts/control_robot.py \
 --control.type=record \
 --control.fps=30 \
 --control.single_task='test_task' \
---control.repo_id=syhlab/my_robot_test_6 \
+--control.repo_id=${recorder}/${exp_name}_${exp_num} \
 --control.num_episodes=1 \
 --control.push_to_hub=false
+--control.warmup_time_s=2 \
+--control.episode_time_s=30 \
+--control.reset_time_s=5
 cd scripts
-# replay
 
+## replay
 cd ..
 python lerobot/scripts/control_robot.py \
 --robot.type=so100 \
 --robot.cameras={} \
 --control.type=replay \
 --control.fps=30 \
---control.repo_id=syhlab/my_robot_test_6 \
+--control.repo_id=${recorder}/${exp_name}_${exp_num}  \
 --control.episode=0 \
 --control.local_files_only=true
 cd scripts
