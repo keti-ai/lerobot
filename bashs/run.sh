e1319a577496b9441ec9272c0fa1303ba09a4f76
syh <syh20131292@gmail.com>
2025-02-04 14:19:02 +0900

    edit bashs

--- bashs/run.sh
+++ bashs/run.sh
@@ -14,24 +14,24 @@
 #--control.type=teleoperate
 #cd scripts
 
-## record
+## record / for configs, check config list on lerobot.common.robot_devices.control_configs.RecordControlConfig
 recorder=syhlab
 exp_name=test_record
-exp_num=1
-cd ..
-python lerobot/scripts/control_robot.py \
---robot.type=so100 \
---robot.cameras={} \
---control.type=record \
---control.fps=30 \
---control.single_task='test_task' \
---control.repo_id=${recorder}/${exp_name}_${exp_num} \
---control.num_episodes=1 \
---control.push_to_hub=false
---control.warmup_time_s=2 \
---control.episode_time_s=30 \
---control.reset_time_s=5
-cd scripts
+exp_num=2
+#cd ..
+#python lerobot/scripts/control_robot.py \
+#--robot.type=so100 \
+#--robot.cameras={} \
+#--control.type=record \
+#--control.fps=30 \
+#--control.single_task='test_task' \
+#--control.repo_id=${recorder}/${exp_name}_${exp_num} \
+#--control.num_episodes=3 \
+#--control.push_to_hub=false \
+#--control.warmup_time_s=2 \
+#--control.episode_time_s=5 \
+#--control.reset_time_s=5
+#cd bashs
 
 ## replay
 cd ..
@@ -41,6 +41,6 @@ python lerobot/scripts/control_robot.py \
 --control.type=replay \
 --control.fps=30 \
 --control.repo_id=${recorder}/${exp_name}_${exp_num}  \
---control.episode=0 \
+--control.episode=2 \
 --control.local_files_only=true
-cd scripts
+cd bashs
