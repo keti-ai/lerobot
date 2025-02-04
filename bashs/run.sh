671a9a499dbc0edb56de6629d52a595cc98bc040
syh <syh20131292@gmail.com>
2025-02-04 14:01:26 +0900

    make bashs

--- bashs/run.sh
+++ bashs/run.sh
@@ -1,46 +1,28 @@
-## cali
-#cd ..
-#python lerobot/scripts/control_robot.py \
-#--robot.type=so100 \
-#--robot.cameras={} \
-#--control.type=calibrate
-#cd scripts
+# cali
 
-## teleoperation
-#cd ..
-#python lerobot/scripts/control_robot.py \
-#--robot.type=so100 \
-#--robot.cameras={} \
-#--control.type=teleoperate
-#cd scripts
+# teleoperation
 
-## record / for configs, check config list on lerobot.common.robot_devices.control_configs.RecordControlConfig
-recorder=syhlab
-exp_name=test_record
-exp_num=3
-#cd ..
-#python lerobot/scripts/control_robot.py \
-#--robot.type=so100 \
-#--robot.cameras={} \
-#--control.type=record \
-#--control.fps=30 \
-#--control.single_task='test_task' \
-#--control.repo_id=${recorder}/${exp_name}_${exp_num} \
-#--control.num_episodes=3 \
-#--control.push_to_hub=false \
-#--control.warmup_time_s=2 \
-#--control.episode_time_s=5 \
-#--control.reset_time_s=5
-#cd bashs
+# record
+cd ..
+python lerobot/scripts/control_robot.py \
+--robot.type=so100 \
+--robot.cameras={} \
+--control.type=record \
+--control.fps=30 \
+--control.single_task='test_task' \
+--control.repo_id=syhlab/my_robot_test_6 \
+--control.num_episodes=1 \
+--control.push_to_hub=false
+cd scripts
+# replay
 
-## replay
 cd ..
 python lerobot/scripts/control_robot.py \
 --robot.type=so100 \
 --robot.cameras={} \
 --control.type=replay \
 --control.fps=30 \
---control.repo_id=${recorder}/${exp_name}_${exp_num}  \
+--control.repo_id=syhlab/my_robot_test_6 \
 --control.episode=0 \
 --control.local_files_only=true
-cd bashs
+cd scripts
