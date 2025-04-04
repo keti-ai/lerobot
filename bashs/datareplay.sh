d17b5c6a97e6ad27315b1e94a8301a4b9cff268d
syhlab <syh20131292@gmail.com>
2025-04-04 16:38:39 +0900

    edit calib

---
+++ bashs/datareplay.sh
@@ -0,0 +1,47 @@
+## record / for configs, check config list on lerobot.common.robot_devices.control_configs.RecordControlConfig
+recorder=syhlab
+exp_name=test_record
+exp_num=$(date +"%Y%m%d_%H%M%S")  # 현재 시간 기반으로 설정됨
+
+#cd ..
+#python lerobot/scripts/control_robot.py \
+#--robot.type=so100 \
+#--robot.cameras='{
+#  "top": {
+#    "type": "intelrealsense",
+#    "serial_number": 918512073045,
+#    "fps": 30,
+#    "width": 640,
+#    "height": 480
+#  }
+#}' \
+#--control.type=record \
+#--control.fps=30 \
+#--control.single_task='test_task' \
+#--control.repo_id=${recorder}/${exp_name}_${exp_num} \
+#--control.num_episodes=3 \
+#--control.push_to_hub=false \
+#--control.warmup_time_s=2 \
+#--control.episode_time_s=5 \
+#--control.reset_time_s=5
+#cd bashs
+
+## replay
+#cd ..
+#python lerobot/scripts/control_robot.py \
+#--robot.type=so100 \
+#--robot.cameras='{
+#  "top": {
+#    "type": "intelrealsense",
+#    "serial_number": 918512073045,
+#    "fps": 30,
+#    "width": 640,
+#    "height": 480
+#  }
+#}' \
+#--control.type=replay \
+#--control.fps=30 \
+#--control.repo_id=${recorder}/${exp_name}_${exp_num}  \
+#--control.episode=2 \
+#--control.local_files_only=true
+#cd bashs
