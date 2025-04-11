11a6e7610115a57c49a2e68588cb505da970e3dd
syhlab <syh20131292@gmail.com>
2025-04-11 19:11:59 +0900

    complete camera errors with original code

--- bashs/datareplay.sh
+++ bashs/datareplay.sh
@@ -1,8 +1,8 @@
 ## record / for configs, check config list on lerobot.common.robot_devices.control_configs.RecordControlConfig
-recorder=syhlab
-exp_name=test_record
-exp_num=$(date +"%Y%m%d_%H%M%S")  # 현재 시간 기반으로 설정됨
-
+#recorder=syhlab
+#exp_name=test_record
+#exp_num=$(date +"%Y%m%d_%H%M%S")  # 현재 시간 기반으로 설정됨
+#
 #cd ..
 #python lerobot/scripts/control_robot.py \
 #--robot.type=so100 \
@@ -26,22 +26,26 @@ exp_num=$(date +"%Y%m%d_%H%M%S")  # 현재 시간 기반으로 설정됨
 #--control.reset_time_s=5
 #cd bashs
 
-## replay
-#cd ..
-#python lerobot/scripts/control_robot.py \
-#--robot.type=so100 \
-#--robot.cameras='{
-#  "top": {
-#    "type": "intelrealsense",
-#    "serial_number": 918512073045,
-#    "fps": 30,
-#    "width": 640,
-#    "height": 480
-#  }
-#}' \
-#--control.type=replay \
-#--control.fps=30 \
-#--control.repo_id=${recorder}/${exp_name}_${exp_num}  \
-#--control.episode=2 \
-#--control.local_files_only=true
-#cd bashs
+recorder=syhlab
+exp_name=test_record
+exp_num=20250411_185907
+
+# replay
+cd ..
+python lerobot/scripts/control_robot.py \
+--robot.type=so100 \
+--robot.cameras='{
+  "top": {
+    "type": "intelrealsense",
+    "serial_number": 918512073045,
+    "fps": 30,
+    "width": 640,
+    "height": 480
+  }
+}' \
+--control.type=replay \
+--control.fps=30 \
+--control.repo_id=${recorder}/${exp_name}_${exp_num}  \
+--control.episode=2 \
+
+
