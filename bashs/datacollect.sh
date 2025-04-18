6b9e69a135bcfb8c7a8cdbc451a4a5f20915f39f
syhlab <syh20131292@gmail.com>
2025-04-18 17:42:36 +0900

    add run_pushT_pi0.sh 1.env setting 2.datacollect 3.convert 4.train 5.eval

--- bashs/datacollect.sh
+++ bashs/datacollect.sh
@@ -1,10 +1,54 @@
 ## record / for configs, check config list on lerobot.common.robot_devices.control_configs.RecordControlConfig
-recorder=syhlab
-exp_name=test_record
-exp_num=$(date +"%Y%m%d_%H%M%S")  # 현재 시간 기반으로 설정됨
+# it confirmed demo dataset collect 250418 syh bellow
+#recorder=syhlab
+#exp_name=pusht
+#exp_num=$(date +"%Y%m%d_%H%M%S")  # 현재 시간 기반으로 설정됨
+#
+#cd ..
+#python lerobot/scripts/control_robot.py \
+#--robot.type=so100 \
+#--robot.cameras='{
+#  "head": {
+#    "type": "intelrealsense",
+#    "serial_number": 918512073045,
+#    "fps": 30,
+#    "width": 1280,
+#    "height": 720
+#  }
+#}' \
+#--control.type=record \
+#--control.fps=30 \
+#--control.single_task='test_task' \
+#--control.repo_id=${recorder}/${exp_name}_${exp_num} \
+#--control.num_episodes=3 \
+#--control.push_to_hub=false \
+#--control.warmup_time_s=2 \
+#--control.episode_time_s=5 \
+#--control.reset_time_s=5 \
+#
+#cd bashs
+#
 
-cd ..
-python lerobot/scripts/control_robot.py \
+# replay demo
+## replay
+#cd ..
+#python lerobot/scripts/control_robot.py \
+#--robot.type=so100 \
+#--robot.cameras={} \
+#--control.type=replay \
+#--control.fps=30 \
+#--control.repo_id=${recorder}/${exp_name}_${exp_num}  \
+#--control.episode=2 \
+#--control.local_files_only=true
+#cd bashs
+
+# 250418 datacollect scripts for push t syh
+export recorder=syhlab
+export exp_name=pusht
+export exp_num=$(date +"%Y%m%d_%H%M%S")
+
+cd lerobot/
+python scripts/control_robot.py \
 --robot.type=so100 \
 --robot.cameras='{
   "head": {
@@ -17,24 +61,10 @@ python lerobot/scripts/control_robot.py \
 }' \
 --control.type=record \
 --control.fps=30 \
---control.single_task='test_task' \
+--control.single_task='push-T-real' \
 --control.repo_id=${recorder}/${exp_name}_${exp_num} \
---control.num_episodes=3 \
+--control.num_episodes=5 \
 --control.push_to_hub=false \
 --control.warmup_time_s=2 \
 --control.episode_time_s=5 \
---control.reset_time_s=5 \
-
-cd bashs
-
-## replay
-#cd ..
-#python lerobot/scripts/control_robot.py \
-#--robot.type=so100 \
-#--robot.cameras={} \
-#--control.type=replay \
-#--control.fps=30 \
-#--control.repo_id=${recorder}/${exp_name}_${exp_num}  \
-#--control.episode=2 \
-#--control.local_files_only=true
-#cd bashs
+--control.reset_time_s=5
