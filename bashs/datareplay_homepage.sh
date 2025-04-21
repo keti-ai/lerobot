2b6dafbc372d29ded3d513c56fe65eac3a824d37
syhlab <syh20131292@gmail.com>
2025-04-21 16:22:05 +0900

    understand single task config parameter

--- bashs/datareplay_homepage.sh
+++ bashs/datareplay_homepage.sh
@@ -4,4 +4,5 @@ exp_name=justmove
 exp_num=20250421_154412
 
 cd ..
-python lerobot/scripts/visualize_dataset_html.py
+python lerobot/scripts/visualize_dataset_html.py \
+  --repo-id=${recorder}/${exp_name}_${exp_num}  \
