b0c7499e625ebd634209c2e65eb7d12ea7f410a2
syh4661 <syh20131292@gmail.com>
2025-04-22 17:35:14 +0900

    gpu error and train error need to be fixed

--- bashs/datareplay_homepage.sh
+++ bashs/datareplay_homepage.sh
@@ -1,8 +1,14 @@
 
 recorder=syhlab
-exp_name=justmove
-exp_num=20250421_154412
+#EXP_NAME=justmove
+#EXP_NUM=20250421_154412
+
+EXP_NAME=move_aroundT
+EXP_NUM=20250421_162801
+REPO_ID=syhlab/${EXP_NAME}_${EXP_NUM}
 
 cd ..
 python lerobot/scripts/visualize_dataset_html.py \
-  --repo-id=${recorder}/${exp_name}_${exp_num}  \
+  --repo-id=syhlab/move_aroundT_20250421_162801 \
+  --host 10.252.205.103 \
+  --port 9090
