54385b368ffd4561d9dd8b59aef54f2eee5840ff
syhlab <syh20131292@gmail.com>
2025-05-09 16:29:06 +0900

    edit data collection with wrist camera

--- bashs/run_collect_data_new.sh
+++ bashs/run_collect_data_new.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # 실험 이름 및 시간 기반 고유 식별자
-EXP_NAME=tape_to_box
+EXP_NAME=move_to_tape
 NOW=$(date '+%Y%m%d_%H%M%S')
 EXP_NUM=${NOW}
 REPO_ID=syhlab/${EXP_NAME}_${EXP_NUM}
 ROBOT_TYPE=so100
-CAMERA_SERIAL=918512073045
+HEAD_CAMERA_SERIAL=918512073045
+HAND_CAMERA_SERIAL=218622278274
 
 # NAS 경로
 NAS_MOUNT_PATH=/mnt/nas/lerobot_shared
@@ -17,17 +18,20 @@ cd ..
 
 python lerobot/scripts/control_robot.py \
   --robot.type=${ROBOT_TYPE} \
-  --robot.cameras="{\"head\": {\"type\": \"intelrealsense\", \"serial_number\": ${CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720}}" \
+  --robot.cameras="{
+    \"head\": {\"type\": \"intelrealsense\", \"serial_number\": ${HEAD_CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720},
+    \"wrist\": {\"type\": \"intelrealsense\", \"serial_number\": ${HAND_CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720}
+  }" \
   --control.type=record \
   --control.fps=30 \
-  --control.single_task="Pick up the circular black object from the left side and insert it into the open box on the right side." \
+  --control.single_task="Move to the tape" \
   --control.repo_id=${REPO_ID} \
   --control.root=${DATASET_DIR} \
-  --control.num_episodes=1 \
+  --control.num_episodes=10 \
   --control.push_to_hub=false \
   --control.warmup_time_s=2 \
-  --control.episode_time_s=20 \
-  --control.reset_time_s=8 \
+  --control.episode_time_s=5 \
+  --control.reset_time_s=5 \
   --control.display_data=true
 
 echo "✅ Data collection done: ${REPO_ID}"
