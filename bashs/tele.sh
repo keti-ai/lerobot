2ba843a747ed68554655c0b5ef160fd530a98d43
syhlab <syh20131292@gmail.com>
2025-05-09 16:15:37 +0900

    add wrist camera

--- bashs/tele.sh
+++ bashs/tele.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 ROBOT_TYPE=so100
-CAMERA_SERIAL=918512073045
+HEAD_CAMERA_SERIAL=918512073045
+HAND_CAMERA_SERIAL=218622278274
 
 echo "🕹️ Starting teleoperation..."
 cd ..
 
 python lerobot/scripts/control_robot.py \
   --robot.type=${ROBOT_TYPE} \
-  --robot.cameras="{\"head\": {\"type\": \"intelrealsense\", \"serial_number\": ${CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720}}" \
+  --robot.cameras="{
+    \"head\": {\"type\": \"intelrealsense\", \"serial_number\": ${HEAD_CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720},
+    \"hand\": {\"type\": \"intelrealsense\", \"serial_number\": ${HAND_CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720}
+  }" \
   --control.type=teleoperate \
   --control.display_data=true
 
