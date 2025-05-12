fd81a64c23997ac7e9b37af676b0b64e317f9dbf
syh4661 <syh20131292@gmail.com>
2025-05-12 15:21:51 +0900

    edit more dataset with wrist cam

--- bashs/run_collect_data_resume.sh
+++ bashs/run_collect_data_resume.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-REPO_ID=syhlab/tape_to_box_20250424_162603
+# 기존 실험 정보 (수정 필요)
+REPO_ID=syhlab/move_to_tape_20250509_162336  # <- 여기를 이전 수집 실험의 REPO_ID로 바꿔주세요
 DATASET_DIR=/mnt/nas/lerobot_shared/datasets/raw/${REPO_ID}
 ROBOT_TYPE=so100
-CAMERA_SERIAL=918512073045
+HEAD_CAMERA_SERIAL=918512073045
+HAND_CAMERA_SERIAL=218622278274
 
 echo "📹 Resuming data recording to: ${DATASET_DIR}"
 cd ..
 
 python lerobot/scripts/control_robot.py \
   --robot.type=${ROBOT_TYPE} \
-  --robot.cameras="{\"head\": {\"type\": \"intelrealsense\", \"serial_number\": ${CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720}}" \
+  --robot.cameras="{
+    \"head\": {\"type\": \"intelrealsense\", \"serial_number\": ${HEAD_CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720},
+    \"wrist\": {\"type\": \"intelrealsense\", \"serial_number\": ${HAND_CAMERA_SERIAL}, \"fps\": 30, \"width\": 1280, \"height\": 720}
+  }" \
   --control.type=record \
   --control.fps=30 \
-  --control.single_task="Pick up the circular black object from the left side and insert it into the open box on the right side." \
+  --control.single_task="Move to the tape" \
   --control.repo_id=${REPO_ID} \
   --control.root=${DATASET_DIR} \
-  --control.num_episodes=10 \
+  --control.num_episodes=30 \
   --control.push_to_hub=false \
   --control.resume=true \
   --control.warmup_time_s=2 \
-  --control.episode_time_s=20 \
-  --control.reset_time_s=8 \
+  --control.episode_time_s=5 \
+  --control.reset_time_s=5 \
   --control.display_data=true
 
 echo "✅ Resume data collection done: ${REPO_ID}"
